0edf4743d55b2de73d2c9d88358046c480e6d3a8
악덕 <rudejrqkr97@gmail.com>
2024-11-11 22:59:20 +0900

    refactor: #1 - data rest 설정
    게시글, 댓글의 json api를 자동으로 restful하게 만들게 설정

--- src/main/java/com/virtue/boardservice/repository/ArticleRepository.java
+++ src/main/java/com/virtue/boardservice/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.virtue.boardservice.repository;
 
 import com.virtue.boardservice.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
